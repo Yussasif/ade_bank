05b259728affefdd4e3d5e00d4bfde989d87dd7d
yussasif <yusufowoade2@gmail.com>
2023-04-07 23:12:11 +0100

    more fix

--- js/script.js
+++ js/script.js
@@ -24,3 +24,25 @@ rewardBtn.addEventListener('click', function(e){
     reward.classList.remove('hidden')
     invest.classList.add('hidden')
 })
+
+const links = document.querySelector('.links')
+const pages = document.querySelectorAll('.page')
+const linkBtn = document.querySelectorAll('.link')
+
+links.addEventListener('click', function(e){
+    e.preventDefault();
+    const link = e.target
+   
+    if(!link.classList.contains('link'))return
+    
+    console.log('link clicked❤')
+
+    linkBtn.forEach(btn => btn === e.target ? btn.closest('.link-item').classList.add('active'): btn.closest('.link-item').classList.remove('active'));
+
+
+    // pages.forEach(page => {
+    //     if()
+    // })
+
+
+})
