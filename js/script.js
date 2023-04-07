eced0c4de85c13eb6928982d37315c2a8874dbf8
yussasif <yusufowoade2@gmail.com>
2023-04-08 00:20:13 +0100

    more fix

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-const home = document.querySelector('.home')
-const reward = document.querySelector('.reward')
-const invest = document.querySelector('.invest')
-
-const homeBtn = document.querySelector('.home-btn')
-const rewardBtn = document.querySelector('.reward-btn')
-const investBtn = document.querySelector('.invest-btn')
-
-homeBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    home.classList.remove('hidden')
-    reward.classList.add('hidden')
-    invest.classList.add('hidden')
-})
-investBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    home.classList.add('hidden')
-    reward.classList.add('hidden')
-    invest.classList.remove('hidden')
-})
-rewardBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    home.classList.add('hidden')
-    reward.classList.remove('hidden')
-    invest.classList.add('hidden')
-})
-
 const links = document.querySelector('.links')
 const pages = document.querySelectorAll('.page')
 const linkBtn = document.querySelectorAll('.link')
@@ -34,15 +7,8 @@ links.addEventListener('click', function(e){
     const link = e.target
    
     if(!link.classList.contains('link'))return
-    
-    console.log('link clicked❤')
 
     linkBtn.forEach(btn => btn === e.target ? btn.closest('.link-item').classList.add('active'): btn.closest('.link-item').classList.remove('active'));
 
-
-    // pages.forEach(page => {
-    //     if()
-    // })
-
-
+    pages.forEach(page => page.dataset.tag === e.target.closest('.link-item').dataset.tag ? page.classList.remove('hidden') : page.classList.add('hidden'))
 })
