6761f402f512df771d4b2b495ffa894cae4342e0
yussasif <yusufowoade2@gmail.com>
2023-04-08 01:42:35 +0100

    more fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
+///////////IMPORTING ELEMENT FROM HTML//////////////
+const popUp = document.querySelector('.pop-up')
+const cancelBtn = document.querySelector('.cancel')
+const okBtn  = document.querySelector('.ok')
+const overlay = document.querySelector('.blur')
+let popUPMessage = document.querySelector('.popup-message')
+const transactionHisBtn = document.querySelector('#transaction-history-btn')
+const transferBtn = document.querySelector('#transfer-btn')
+const loanBtn = document.querySelector('#loan-btn')
+const withdrawBtn = document.querySelector('#withdraw-btn')
 const links = document.querySelector('.links')
 const pages = document.querySelectorAll('.page')
 const linkBtn = document.querySelectorAll('.link')
+console.log(popUPMessage)
+
+///////////////POP UP///////////////
+
+///OPEN POP UP///
+const openPopUp = function (){
+    popUp.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+///CLOSE POPUP///
+const closePupUp = function (){
+    popUp.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+///ADDING EVENT LISTENER TO CLOSE POP UP BUTTONS
+const closePopUpbBtns = [overlay, cancelBtn, okBtn]
+
+closePopUpbBtns.forEach(btn => btn.addEventListener('click', closePupUp))
+
+////////PENDING FUNCTIONALITIES WARNING ON CLICK////////
+const pendings = [transactionHisBtn, transferBtn, loanBtn, withdrawBtn]
+pendings.forEach((pending) => pending.addEventListener('click', function(){
+    openPopUp()
+    popUPMessage.textContent = 'This functionality has not yet been implemented. Check back later'
+    console.log(popUPMessage.textContent)
+}))
+
+
+//////////////NAVIGATE PAGES///////////////
 
 links.addEventListener('click', function(e){
     e.preventDefault();
@@ -12,3 +52,4 @@ links.addEventListener('click', function(e){
 
     pages.forEach(page => page.dataset.tag === e.target.closest('.link-item').dataset.tag ? page.classList.remove('hidden') : page.classList.add('hidden'))
 })
+
